cf9cf7ed3e36c7cdf579db408f37e4d384f30da9
aman-webdev <aman.nabi02@gmail.com>
2022-07-13 06:29:03 +0530

    test

--- test/test.js
+++ test/test.js
@@ -19,6 +19,15 @@ describe("CRUD", () => {
   });
 
   describe("Read User", () => {
-    it("");
+    it("should get info of the added user", async () => {
+      const user = await crud.addUser("James");
+      const [id, name] = await crud.getUser(1);
+      assert.equal(id.toString(), "1");
+      assert.equal(name, "James");
+    });
+
+    it("Should revert if the user with an id does not exist", async () => {
+      await expect(crud.getUser(1)).to.be.reverted;
+    });
   });
 });
