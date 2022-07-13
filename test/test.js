5c58cc2f94cdd28cecd8c2e595c94779b1407811
aman-webdev <aman.nabi02@gmail.com>
2022-07-13 06:40:34 +0530

    completes unit test

--- test/test.js
+++ test/test.js
@@ -30,4 +30,28 @@ describe("CRUD", () => {
       await expect(crud.getUser(1)).to.be.reverted;
     });
   });
+
+  describe("Update User", () => {
+    it("Should update the user", async () => {
+      const user = await crud.addUser("James");
+      const newUser = await crud.updateUser(1, "Thomas");
+      const [id, name] = await crud.getUser(1);
+      assert.equal(name, "Thomas");
+    });
+
+    it("Should not update user if the user does not exist", async () => {
+      await expect(crud.updateUser(1, "Thomas  ")).to.be.reverted;
+    });
+  });
+
+  describe("Delete User", () => {
+    it("Should delete user", async () => {
+      const user = await crud.addUser("Thomas");
+      const [id] = await crud.getUser(1);
+
+      await crud.deleteUser(1);
+      assert.equal(id.toString(), "1");
+      await expect(crud.getUser(1)).to.be.reverted;
+    });
+  });
 });
